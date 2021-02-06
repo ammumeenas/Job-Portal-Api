d5da0674919fa5fa69f85fc46610ad55602380e0
Your Name <ammumeenas@gmail.com>
2021-02-06 15:25:37 -0600

    created CandidateProfile model

--- JobPortalAPI/Models/JobSkill.cs
+++ JobPortalAPI/Models/JobSkill.cs
@@ -3,9 +3,6 @@ namespace JobPortalAPI.Models
 {
     public class JobSkill
     {
-        public JobSkill(int _SkillId) {
-            SkillId = _SkillId;
-        }
         public Job Job { get; set; }
         public int JobId { get; set; }
         public Skill Skill { get; set; }
---
+++ JobPortalAPI/Models/candidateProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JobPortalAPI.Models
+{
+    public class candidateProfile
+    {
+        public string id { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public int Experience { get; set; }
+        public List<Skill> Skills { get; set; 
+
+
+
+
+    }
+}
